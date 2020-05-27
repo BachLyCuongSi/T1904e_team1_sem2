c37f7790e658cc55f04be5aea5bf12f8ef055894
Bach-ly-cuong-sinh <54472158+Bach-ly-cuong-sinh@users.noreply.github.com>
2020-05-27 14:52:51 +0700

    an

--- Project/resources/views/user/list.blade.php
+++ Project/resources/views/user/list.blade.php
@@ -1,13 +1,13 @@
 
-		
-	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 		<div class="row">
 			<ol class="breadcrumb">
 				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
 				<li class="active">Danh sách thành viên</li>
 			</ol>
 		</div><!--/.row-->
-		
+
 		<div class="row">
 			<div class="col-lg-12">
 				<h1 class="page-header">Danh sách thành viên</h1>
@@ -22,7 +22,7 @@
 			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-body">
-                        <table 
+                        <table
                             data-toolbar="#toolbar"
                             data-toggle="table">
 
@@ -72,10 +72,10 @@
                     </div>
 				</div>
 			</div>
-		</div><!--/.row-->	
+		</div><!--/.row-->
 	</div>	<!--/.main-->
 
 	<script src="js/jquery-1.11.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-    <script src="js/bootstrap-table.js"></script>	
+    <script src="js/bootstrap-table.js"></script>
 
