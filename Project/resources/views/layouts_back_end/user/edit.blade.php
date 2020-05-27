558f1479f6842f7b44ccc300f2e60c2fd4d90434
Bach-ly-cuong-sinh <54472158+Bach-ly-cuong-sinh@users.noreply.github.com>
2020-05-27 14:56:23 +0700

    an

--- Project/resources/views/layouts_back_end/user/edit.blade.php
+++ Project/resources/views/layouts_back_end/user/edit.blade.php
@@ -1,17 +1,17 @@
 
-		
-	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 		<div class="row">
 			<ol class="breadcrumb">
                 <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
                 <li><a href="">Quản lý thành viên</a></li>
-				<li class="active">Thêm thành viên</li>
+				<li class="active">Nguyễn Văn A</li>
 			</ol>
 		</div><!--/.row-->
-		
+
 		<div class="row">
 			<div class="col-lg-12">
-				<h1 class="page-header">Thêm thành viên</h1>
+				<h1 class="page-header">Thành viên: Nguyễn Văn A</h1>
 			</div>
         </div><!--/.row-->
         <div class="row">
@@ -19,32 +19,32 @@
                     <div class="panel panel-default">
                         <div class="panel-body">
                             <div class="col-md-8">
-                            	<div class="alert alert-danger">Email đã tồn tại !</div>
-                                <form role="form" method="post">
+                                <div class="alert alert-danger">Email đã tồn tại, Mật khẩu không khớp !</div>
+                            <form role="form" method="post">
                                 <div class="form-group">
                                     <label>Họ & Tên</label>
-                                    <input name="user_full" required class="form-control" placeholder="">
+                                    <input type="text" name="user_full" required class="form-control" value="Nguyễn Văn A" placeholder="">
                                 </div>
                                 <div class="form-group">
                                     <label>Email</label>
-                                    <input name="user_mail" required type="text" class="form-control">
-                                </div>                       
+                                    <input type="text" name="user_mail" required value="nguyenvana@gmail.com" class="form-control">
+                                </div>
                                 <div class="form-group">
                                     <label>Mật khẩu</label>
-                                    <input name="user_pass" required type="password"  class="form-control">
+                                    <input type="password" name="user_pass" required  class="form-control">
                                 </div>
                                 <div class="form-group">
                                     <label>Nhập lại mật khẩu</label>
-                                    <input name="user_re_pass" required type="password"  class="form-control">
+                                    <input type="password" name="user_re_pass" required  class="form-control">
                                 </div>
                                 <div class="form-group">
                                     <label>Quyền</label>
                                     <select name="user_level" class="form-control">
                                         <option value=1>Admin</option>
-                                        <option value=2>Member</option>
+                                        <option value=2 selected>Member</option>
                                     </select>
                                 </div>
-                                <button name="sbm" type="submit" class="btn btn-success">Thêm mới</button>
+                                <button type="submit" name="sbm" class="btn btn-primary">Cập nhật</button>
                                 <button type="reset" class="btn btn-default">Làm mới</button>
                             </div>
                         </form>
@@ -52,6 +52,5 @@
                     </div>
                 </div><!-- /.col-->
             </div><!-- /.row -->
-		
-	</div>	<!--/.main-->	
 
+	</div>	<!--/.main-->
