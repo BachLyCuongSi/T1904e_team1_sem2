cf0fa1b8be02598a32350bd0ddf7c6a2af6189b4
Bach-ly-cuong-sinh <54472158+Bach-ly-cuong-sinh@users.noreply.github.com>
2020-05-27 15:01:31 +0700

    ann

--- Project/resources/views/layouts_back_end/category/add.blade.php
+++ Project/resources/views/layouts_back_end/category/add.blade.php
@@ -1,6 +1,6 @@
 
-		
-	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 		<div class="row">
 			<ol class="breadcrumb">
 				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
@@ -8,7 +8,7 @@
 				<li class="active">Thêm danh mục</li>
 			</ol>
 		</div><!--/.row-->
-		
+
 		<div class="row">
 			<div class="col-lg-12">
 				<h1 class="page-header">Thêm danh mục</h1>
@@ -32,5 +32,5 @@
                     </div>
                 </div>
             </div><!-- /.col-->
-	</div>	<!--/.main-->	
+	</div>	<!--/.main-->
 
