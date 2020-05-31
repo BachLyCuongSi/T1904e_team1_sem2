d9fb821ceb700bcb6fbcc162232f6283854a8434
Bach-ly-cuong-sinh <54472158+Bach-ly-cuong-sinh@users.noreply.github.com>
2020-05-31 10:56:54 +0700

    an

---
+++ Project/resources/views/font_end/modules/slide/slide_tap.php
@@ -0,0 +1,11 @@
+<div class="hero-wrap hero-bread" style="background-image: url('images/bg_1.jpg');">
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate text-center">
+                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Checkout</span></p>
+                <!-- khi thay đổi đường link giá trị h1 thay đổi theo dùng cho các tap menu -->
+                <h1 class="mb-0 bread">Checkout</h1>
+            </div>
+        </div>
+    </div>
+</div>
