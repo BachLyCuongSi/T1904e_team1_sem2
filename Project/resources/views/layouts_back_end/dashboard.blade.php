2db53ecd3d73f793414e192f1fb96e702073d5c8
Bach-ly-cuong-sinh <54472158+Bach-ly-cuong-sinh@users.noreply.github.com>
2020-06-03 23:52:39 +0700

    Update dashboard.blade.php
    an

--- Project/resources/views/layouts_back_end/dashboard.blade.php
+++ Project/resources/views/layouts_back_end/dashboard.blade.php
@@ -2,136 +2,89 @@
 
 @section('content')
 {{-- \container-fluid --}}
-<div class="col-sm-10 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+<div class="">
      <div class="container-fluid">
          <div class="row">
-                    <div class="col-lg-10">
-                        <h1 class="page-header">Dashboard</h1>
-                    </div>
-                    <!-- /.col-lg-12 -->
+            <div class="col-lg-10">
+                <h1 class="page-header">Dashboard</h1>
+            </div>
+            <!-- /.col-lg-12 -->
          </div>
          <!-- /.row -->
          <div class="row">
-                    <div class="col-lg-3 col-md-6">
-                        <div class="panel panel-primary">
-                            <div class="panel-heading">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        <i class="fa fa-comments fa-3x"></i>
-                                    </div>
-                                    <div class="col-xs-9 text-right">
-                                        <div class="huge">26</div>
-                                        <div>New Comment!</div>
-                                    </div>
-                                </div>
+            <div   class="col-lg-3 col-md-6">
+                <div   class="panel panel-primary">
+                    {{-- panel --}}
+                    <div style="height: 15%" class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-comments fa-5x"></i>
                             </div>
-                            <a href="#">
-                                <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                                    <div class="clearfix"></div>
-                                </div>
-                            </a>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">26</div>
+                                <div>New Comment!</div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="panel panel-green">
-                            <div class="panel-heading">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        <i class="fa fa-tasks fa-5x"></i>
-                                    </div>
-                                    <div class="col-xs-9 text-right">
-                                        <div class="huge">12</div>
-                                        <div>New Tasks!</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="#">
-                                <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
 
-                                    <div class="clearfix"></div>
-                                </div>
-                            </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-green">
+                    <div style="height: 15%" class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-tasks fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">12</div>
+                                <div>New Tasks!</div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="panel panel-yellow">
-                            <div class="panel-heading">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        <i class="fa fa-shopping-cart fa-5x"></i>
-                                    </div>
-                                    <div class="col-xs-9 text-right">
-                                        <div class="huge">124</div>
-                                        <div>New Orders!</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="#">
-                                <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
 
-                                    <div class="clearfix"></div>
-                                </div>
-                            </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-yellow ">
+                    <div style="height: 15%" class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-shopping-cart fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">124</div>
+                                <div>New Orders!</div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6">
-                        <div class="panel panel-red">
-                            <div class="panel-heading">
-                                <div class="row">
-                                    <div class="col-xs-3">
-                                        <i class="fa fa-support fa-5x"></i>
-                                    </div>
-                                    <div class="col-xs-9 text-right">
-                                        <div class="huge">13</div>
-                                        <div>Support Tickets!</div>
-                                    </div>
-                                </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div   class="panel panel-red">
+                    <div style="height: 15%" class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-support fa-5x"></i>
                             </div>
-                            <a href="#">
-                                <div class="panel-footer">
-                                    <span class="pull-left">View Details</span>
-                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                                    <div class="clearfix"></div>
-                                </div>
-                            </a>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">13</div>
+                                <div>Support Tickets!</div>
+                            </div>
                         </div>
                     </div>
+                </div>
+            </div>
          </div>
+        <hr>
+
          <!-- /.row -->
          <div class="row">
                     <div class="col-lg-8">
-
                         <!-- /.panel -->
                         <div class="panel panel-default">
                             <div class="panel-heading">
                                 <i class="fa fa-bar-chart-o fa-fw"></i> Bar Chart Example
-                                <div class="pull-right">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
-                                            Actions
-                                            <span class="caret"></span>
-                                        </button>
-                                        <ul class="dropdown-menu pull-right" role="menu">
-                                            <li><a href="#">Action</a>
-                                            </li>
-                                            <li><a href="#">Another action</a>
-                                            </li>
-                                            <li><a href="#">Something else here</a>
-                                            </li>
-                                            <li class="divider"></li>
-                                            <li><a href="#">Separated link</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
                             </div>
                             <!-- /.panel-heading -->
                             <div class="panel-body">
@@ -141,37 +94,14 @@
                                             <table class="table table-bordered table-hover table-striped">
                                                 <thead>
                                                     <tr>
-                                                        <th>#</th>
-                                                        <th>Date</th>
-                                                        <th>Time</th>
-                                                        <th>Amount</th>
+                                                        <th>Đơn hàng</th>
+                                                        <th>Ngày</th>
+                                                        <th>Giờ</th>
+                                                        <th>Tổng tiền</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    <tr>
-                                                        <td>3326</td>
-                                                        <td>10/21/2013</td>
-                                                        <td>3:29 PM</td>
-                                                        <td>$321.33</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>3325</td>
-                                                        <td>10/21/2013</td>
-                                                        <td>3:20 PM</td>
-                                                        <td>$234.34</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>3324</td>
-                                                        <td>10/21/2013</td>
-                                                        <td>3:03 PM</td>
-                                                        <td>$724.17</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>3323</td>
-                                                        <td>10/21/2013</td>
-                                                        <td>3:00 PM</td>
-                                                        <td>$23.71</td>
-                                                    </tr>
+
                                                     <tr>
                                                         <td>3322</td>
                                                         <td>10/21/2013</td>
@@ -267,7 +197,6 @@
     <!-- /#page-wrapper -->
 </div>
 
-
 <!-- /#wrapper -->
 <!-- jQuery -->
 <script src="../js/jquery.min.js"></script>
@@ -284,6 +213,6 @@
 
 
 
-
+@endsection
 
 
