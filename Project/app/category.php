65f65644179e3da06709e326fb5ff8e276ab037f
hoangmanh1999 <54472253+hoangmanh1999@users.noreply.github.com>
2020-06-05 23:07:32 +0700

    up

--- Project/app/category.php
+++ Project/app/category.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class category extends Model
 {
     protected $primaryKey = 'cat_id';
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
 }
