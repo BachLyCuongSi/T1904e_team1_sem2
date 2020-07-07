974d0fffcfaedaca45be45fc9475b3595b189dd1
longbot2703 <hoanglong2703@gmail.com>
2020-07-07 09:20:58 +0700

    Update paypal.php

--- Project/config/paypal.php
+++ Project/config/paypal.php
@@ -1,7 +1,7 @@
 <?php
-return{
-  'client_id' => 'ARNkjiY51SXOBTngfP8ikdalpMAhEgyFpdVwabxG9rt700zDPv_PAV9JxKqi_VCz5SW0rgRz16CVSMYW';
-  'secret' => 'EBseVsGqfPPRhUs04B_1o-MnRnDe6HGx24NbFeAHcGe3fSyKnng-yScx2UlRp3pFpsvprpgY-KeHIJZC';
+return [
+  'client_id' => 'ARNkjiY51SXOBTngfP8ikdalpMAhEgyFpdVwabxG9rt700zDPv_PAV9JxKqi_VCz5SW0rgRz16CVSMYW',
+  'secret' => 'EBseVsGqfPPRhUs04B_1o-MnRnDe6HGx24NbFeAHcGe3fSyKnng-yScx2UlRp3pFpsvprpgY-KeHIJZC',
   'setting' => [
     'http.CURLOPT_CONNECTIMEOUT' => 1200,
     'mode' => 'sanbox',
@@ -9,5 +9,5 @@ return{
     'log.FileName' => storage_path().'/logs/paypal.php',
     'log.LogEvent' => 'FINE',
   ]
-}
+]
 ?>
